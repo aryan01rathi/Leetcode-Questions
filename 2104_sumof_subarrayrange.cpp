90bf979faf4ede022b172974200909f48d530c52
Aryan Rathi <132299830+aryan01rathi@users.noreply.github.com>
2023-06-19 00:28:56 +0530

    Add files via upload

---
+++ 2104_sumof_subarrayrange.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long subArrayRanges(vector<int>& nums) {
+       int n=nums.size();
+       long long sum=0;
+     
+       for(int i=0;i<n;i++){
+            int l=INT_MIN;
+       int s=INT_MAX;
+           for( int j=i;j<n;j++){
+               l=max(l,nums[j]);
+               s=min(s,nums[j]);
+               sum+=(l-s);
+           }
+       }
+       return sum;
+     
+    } 
+int main(){
+    vector<int>v={1,2,3};
+    cout<<subArrayRanges(v);
+}
