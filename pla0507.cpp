11d504afad86e32f37ecb092e80bcb9173e5f54a
Aryan Rathi <aryan2001rathi@gmail.com>
2023-07-05 13:05:32 +0530

    pla

--- pla0507.cpp
+++ pla0507.cpp
@@ -19,15 +19,37 @@ void leaderInArray(vector<int> v)
     }
 }
 
+void kelementsCloser(vector<int>v,int k, int x){
+    sort(v.begin(), v.end());
+    int i=0;
+    int j=v.size()-1;
+    vector<int>ans;
+    while(i<=j && k>0){
+        if(abs(v[i]-x)<=abs(v[j]-x)){
+            ans.push_back(v[i]);
+            i++;
+            
+        }
+        else{
+            ans.push_back(v[j]);
+            j--;
+        }
+        k--;
+    }
+    for(auto it :ans){
+        cout<<it<<" ";
+    }
+}
+
 
 
 
 int main()
 {
     vector<int> v = {5,3,2,4,2,1};
-    leaderInArray(v);
+    //leaderInArray(v);
     
-   
+    kelementsCloser(v,4,3); //{2,4,7,9,10}
 
   
 
