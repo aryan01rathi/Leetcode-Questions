a04be2680284eb178070031d2d20dc70f0eab893
Aryan Rathi <aryan2001rathi@gmail.com>
2023-07-22 13:05:32 +0530

    cp

---
+++ chefbarber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    
+    cout<<n*m<<endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
