3bed9b6b51276421d1b36ff9c05724e17e3b80d0
Aryan Rathi <aryan2001rathi@gmail.com>
2023-07-24 13:05:32 +0530

    conversion

---
+++ posttopre.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool ischar(char ch){
+    switch(ch){
+        case '+':
+        case '-':
+        case '/':
+       
+        case '*':
+        return true;
+    }
+    return false;
+}
+
+string post_to_pre(string str){
+
+    stack<string> st;
+    int len=str.size();
+
+    for(int i=0;i<len;i++){
+
+        if(ischar(str[i])){
+            string temp1=st.top();
+            st.pop();
+            string temp2= st.top();
+            st.pop();
+            string result=str[i]+temp2+temp1;
+            st.push(result);
+        }
+        else{
+            //this syntax creating a string of length 1 given as the first argument
+            //and the next argument specifies the char to be used as the new string.
+            //string(1, str[i])
+            
+            st.push(string(1,str[i]));
+             
+        }
+    }
+    return st.top();
+}
+
+int main(){
+    string s="ABC/-AK/L-*";
+    cout<<post_to_pre(s);
+
+}
