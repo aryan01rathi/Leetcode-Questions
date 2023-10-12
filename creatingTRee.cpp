159d544ddf0a8341ec465320360903c52176f3f5
Aryan Rathi <aryan2001rathi@gmail.com>
2023-10-12 22:56:33 +0530

    committ

--- creatingTRee.cpp
+++ creatingTRee.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node{
@@ -30,8 +31,66 @@ node* buildTree(node *root){
     return root;
 }
 
+void levelOrderTraversal(node *root){
+    queue<node*>q;
+    q.push(root);
+    //to make sure the tree is not printed in a single line
+    q.push(NULL);
+
+    while(!q.empty()){
+        node* temp=q.front();
+        q.pop();
+        if(temp==NULL){
+            cout<<endl;
+            //to make sure after all the node of same level are printed next level is printed in the next line
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+void inOrder(node * root){
+    if(root==NULL){return;}
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+void preOrder(node * root){
+    if(root==NULL){return;}
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    
+    preOrder(root->right);
+}
+
+void postOrder(node * root){
+    if(root==NULL){return;}
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+
 int main(){
     node* root=NULL;
     root=buildTree(root);
-    
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1 
+    levelOrderTraversal(root);
+    cout<<"Inorder traversal "<<endl;
+    inOrder(root);
+    cout<<endl;
+    cout<<"Preorder traversal "<<endl;
+    preOrder(root);
+    cout<<endl;
+    cout<<"Postorder traversal "<<endl;
+    postOrder(root);
 }
