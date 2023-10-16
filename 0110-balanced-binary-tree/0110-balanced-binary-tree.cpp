0e9c675770eaef436c0f4c244b6741e044707653
Aryan Rathi <132299830+aryan01rathi@users.noreply.github.com>
2023-10-16 15:40:52 +0530

    Time: 4 ms (93.29%), Space: 21.1 MB (48.93%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -19,19 +19,34 @@ public:
         int rh=depth(root->right);
         return 1+max(lh,rh);
     }
-    bool isBalanced(TreeNode* root) {
-        
+    
+    int height(TreeNode * root){
         if(root==NULL){
-            return true;
-        
+            return 0;
         }
         
-        int lefth=depth(root->left);
-        int righth=depth(root->right);
+        int LH=height(root->left);
+        if(LH==-1)return -1;
+        int RH=height(root->right);
+        if(RH==-1)return -1;
         
-        if((abs(lefth-righth)<=1) && isBalanced(root->left) && isBalanced(root->right)){
-            return true;
-        }
-        return false;
+        if(abs(RH-LH)>1)return -1;
+        return 1+max(LH,RH);
+    }
+    bool isBalanced(TreeNode* root) {
+        //we are checking the height of leftsubtree(lS) and rightsubtree(RS) and if the difference is less than 1 then we do the same process for other nodes. O(n^2)
+//         if(root==NULL){
+//             return true;
+        
+//         }
+//         int lefth=depth(root->left);
+//         int righth=depth(root->right);
+        
+//         if((abs(lefth-righth)<=1) && isBalanced(root->left) && isBalanced(root-                         >right)){
+//             return true;
+//         }
+//         return false;
+        //--------------------------------
+        return height(root)!=-1;
     }
 };
