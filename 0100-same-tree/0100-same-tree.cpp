b20e8baf94103c34d817d70de4d83a1489c54f36
Aryan Rathi <132299830+aryan01rathi@users.noreply.github.com>
2023-10-25 16:59:32 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (7.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,32 +11,49 @@
  */
 class Solution {
 public:
-    void traversal(TreeNode* p, vector<char>&vp){
-        if(p==NULL){
-            vp.push_back('N');
+//     void traversal(TreeNode* p, vector<char>&vp){
+//         if(p==NULL){
+//             vp.push_back('N');
+//             return;
+//         }
+//         int data=p->val;
+        
+//         char cv= static_cast<char>(data+'0');
+     
+//         vp.push_back(cv);
+//         traversal(p->left,vp);
+//         traversal(p->right,vp);
+        
+//     }
+    
+    void traversalcheck(TreeNode* p, TreeNode* q, bool &check){
+        if(p==NULL || q==NULL){
+            if(p!=q)check=false;
             return;
         }
-        int data=p->val;
-        
-        char cv= static_cast<char>(data+'0');
-     
-        vp.push_back(cv);
-        traversal(p->left,vp);
-        traversal(p->right,vp);
-        
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<char>vp;
-        vector<char>vq;
-        traversal(p,vp);
-        traversal(q,vq);
-        for(int i=0;i<vq.size();i++){
-            if(vp[i]!=vq[i]){
-                return false;
-            }
+        if(p->val!=q->val){
+            check=false;
+            
         }
+        traversalcheck(p->left,q->left,check);
+        traversalcheck(p->right,q->right,check);        
         
-        return true;
+       }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+//         vector<char>vp;
+//         vector<char>vq;
+//         traversal(p,vp);
+//         traversal(q,vq);
+//         for(int i=0;i<vq.size();i++){
+//             if(vp[i]!=vq[i]){
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+        bool check=true;
+        traversalcheck(p,q,check);
+        return check;
         
     }
 };
