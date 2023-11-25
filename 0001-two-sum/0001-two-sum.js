96c7ab36bf73920e8c81f280eb595b167b8e03a0
Aryan Rathi <132299830+aryan01rathi@users.noreply.github.com>
2023-11-26 02:48:14 +0530

    Time: 62 ms (60.47%), Space: 43.2 MB (25.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,37 +4,53 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let left=0;
-    let right=nums.length-1;
-    let ansarray=[];
+//     let left=0;
+//     let right=nums.length-1;
+//     let ansarray=[];
     
-    temparra=nums.slice().sort((a,b)=>a-b)
+//     temparra=nums.slice().sort((a,b)=>a-b)
     
-    while(left<right){
-       //console.log("yes")
-        if(temparra[left]+temparra[right]===target){
-            if(temparra[left]!==temparra[right]){
-            let index1=nums.indexOf(temparra[left])
-            let index2=nums.indexOf(temparra[right])
+//     while(left<right){
+//        //console.log("yes")
+//         if(temparra[left]+temparra[right]===target){
+//             if(temparra[left]!==temparra[right]){
+//             let index1=nums.indexOf(temparra[left])
+//             let index2=nums.indexOf(temparra[right])
             
-            ansarray.push(index1);
-            ansarray.push(index2);}
-        else{
-                nums.forEach((element, index) => {
-                        if (temparra[left] === element) {
-                            ansarray.push(index);
-                        }   
-                });
-            }
-            break;
+//             ansarray.push(index1);
+//             ansarray.push(index2);}
+//         else{
+//                 nums.forEach((element, index) => {
+//                         if (temparra[left] === element) {
+//                             ansarray.push(index);
+//                         }   
+//                 });
+//             }
+//             break;
            
-        }
-        else if(temparra[left]+temparra[right]>=target){
-            right--;
-        }
-        else if(temparra[left]+temparra[right]<=target){
-            left++;
+//         }
+//         else if(temparra[left]+temparra[right]>=target){
+//             right--;
+//         }
+//         else if(temparra[left]+temparra[right]<=target){
+//             left++;
+//         }
+//     }
+//     return ansarray;
+    
+    
+    let map=new Map();
+    
+    for(let i=0;i<nums.length;i++){
+        if(map.has(target-nums[i])){
+            return [map.get(target-nums[i]),i]
+        }else{
+            map.set(nums[i],i);
         }
     }
-    return ansarray;
+    
+    
+    return [];
+    
+    
 };
